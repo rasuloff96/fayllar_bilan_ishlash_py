3eac5fda80a752e4dc241036a0ea884631d29ab6
Asilbek <asildev96@gmail.com>
2026-02-17 12:09:31 +0500

    created files

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import platform
+import json
+import shutil
+import subprocess
+from datetime import datetime
+
+
+# Papka yaratish uchun os modulidan foydalanildi.
+
+folder_name = "chiquvchi_fayllar"
+if not os.path.exists(folder_name):
+    os.mkdir(folder_name)
+    print(f"{folder_name} nomli papka yaratildi.")
+else:
+    print(f"{folder_name} nomli papka allaqachon mavjud.")
+
+file_path = os.path.join(folder_name, "malumotlar.txt")
+
+files = os.listdir(folder_name)
+print("Papkadagi fayllar:" , files)
+
+
+
+with open(file_path, "w") as file:
+    file.write( "Protsessor malumoti: " + platform.processor() + "\n")
+    file.write( "Operatsion tizim: " + platform.system() + "\n")
+    file.write( "Tizim versiyasi: " + platform.release() + "\n")
+    file.write( "Platforma versiyasi: " + platform.version() + "\n")
+    
