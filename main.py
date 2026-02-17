6ea31d98cb1d81c7575ba4a7c81dbece1fb28b8b
Asilbek <asildev96@gmail.com>
2026-02-17 12:35:27 +0500

    Add functionality to sort and save PC data by RAM into a new folder

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import subprocess
 from datetime import datetime
 
 
+
 # Papka yaratish uchun os modulidan foydalanildi.
 
 folder_name = "chiquvchi_fayllar"
@@ -22,7 +23,38 @@ print("Papkadagi fayllar:" , files)
 
 
 
-with open(pcs.json, "r") as file:
+# JSON faylini o'qish va ma'lumotlarni RAM bo'yicha saralash
+
+with open('pcs.json', "r") as file:
     all_pcs = json.load(file)
     
+sorted_pcs = sorted(all_pcs, key=lambda pcs: pcs["ram"], reverse=True)
+
+filtered_folder = "saralangan_malumotlar"
+
+
+
+# Yangi papka yaratish yoki mavjudligini tekshirish
+
+if not os.path.exists(filtered_folder):
+    os.mkdir(filtered_folder)
+    print(f"{filtered_folder} nomli papka yaratildi.")
+else:
+    print(f"{filtered_folder} nomli papka allaqachon mavjud.")
     
+    
+
+# Saralangan ma'lumotlarni yangi papkaga yozish
+
+file_path = os.path.join(filtered_folder, "saralangan_malumotlar.txt")
+with open(file_path, "w", encoding="utf-8") as f:
+    f.write("Saralangan ma'lumotlar (RAM bo'yicha):\n")
+    f.write("=" * 50 + "\n")
+    
+    for index , pc in enumerate(sorted_pcs, start=1):
+        f.write(f"{index}. Model: {pc['model']}\n")
+        f.write(f"   RAM: {pc['ram']} GB\n")
+        f.write(f"   Storage: {pc['storage']} GB\n")
+        f.write("-" * 50 + "\n")
+        
+print(f"Saralangan ma'lumotlar '{file_path}' fayliga yozildi.")
